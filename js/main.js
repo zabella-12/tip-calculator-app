b492d3af9360ba145ea9486bd97aa2462b330c56
zabella-12 <issaasousa@outlook.com>
2023-02-26 15:48:06 -0300

    refatoração cálculo comissão

--- js/main.js
+++ js/main.js
@@ -1,55 +1,59 @@
-let totalConta = document.querySelector('#number-check')
-let listaDeBotao = document.querySelectorAll('.percent')
-let comissaoCustom = document.querySelector('.custom')
-let numPessoa = document.querySelector('#number-people').value;
-let sumPerson = document.querySelector('#tip-person').innerHTML = '0.00'
-let totalPerson = document.querySelector('#total').innerHTML = '0.00'
+let totalConta = document.querySelector('#number-check');
+let listaDeBotao = document.querySelectorAll('.percent');
+let comissaoCustom = document.querySelector('.custom');
+let numPessoa = document.querySelector('#number-people');
+let sumPerson = document.querySelector('#tip-person');
+let totalPerson = document.querySelector('#total');
 
-addEventListener('change', () => {
-    totalConta = document.querySelector('#number-check').value
-    totalConta = parseFloat(totalConta)
-})
 
-addEventListener('change', () => {
-    numPessoa = document.querySelector('#number-people').value
-    numPessoa = parseInt(numPessoa)
+//calculo de comissões
+function calcularValores(comissao) {
+    const qtdPessoa = parseFloat(totalConta.value) / parseInt(numPessoa.value);
+    const checkPerson = (qtdPessoa * comissao).toFixed(2);
+    const checkTotal = (qtdPessoa * comissao + qtdPessoa).toFixed(2);
 
-    if (numPessoa == 0) {
-        const validaPessoa = document.querySelector('.messagen').innerHTML = `Can't be zero`
-        input.toggle('focus-color');
+    sumPerson.textContent = checkPerson;
+    totalPerson.textContent = checkTotal;
+}
+
+//capitura de eventos
+numPessoa.addEventListener('focusout', () => {
+    const nPessoa = parseInt(numPessoa.value);
+
+    if (nPessoa <= 0) {
+        document.querySelector('.messagen').textContent = "Can't be zero";
+        numPessoa.classList.add('focus-color');
+    } else {
+        document.querySelector('.messagen').textContent = '';
+        numPessoa.classList.remove('focus-color');
     }
+
+    sumPerson.textContent = '0.00';
+    totalPerson.textContent = '0.00';
+});
+
+totalConta.addEventListener('focusout', () => {
+    sumPerson.textContent = '0.00';
+    totalPerson.textContent = '0.00';
 })
+
+//verificar qual botão de comissão foi clicado
 
 for (let i = 0; i < listaDeBotao.length; i++) {
     const botaoComissao = listaDeBotao[i];
     const valorComissao = parseInt(botaoComissao.value);
-    const idBotaoComissao = `#valor_${valorComissao}`;
 
-    botaoComissao.onclick = function () {
-        comissaoFixa(idBotaoComissao)
+    botaoComissao.onclick = () => {
+
+        //passagem de comissão de fixa
+        const comissao = valorComissao / 100;
+        calcularValores(comissao);
     }
-
-    function comissaoFixa(idBotaoClicado) {
-        document.querySelector(idBotaoClicado).addEventListener('click', () => {
-            const comissao = valorComissao / 100
-            const qtdPessoa = totalConta / numPessoa
-            let checkPerson = qtdPessoa * comissao
-            sumPerson = document.querySelector('#tip-person').innerHTML = checkPerson.toFixed(2)
-            let checkTotal = (qtdPessoa * comissao) + qtdPessoa
-            totalPerson = document.querySelector('#total').innerHTML = checkTotal.toFixed(2)
-        })
-    }
-
 }
 
-addEventListener('change', () => {
-    comissaoCustom = document.querySelector('.custom').value;
-    comissaoCustom = parseFloat(comissaoCustom)
+//comissão customizada
 
-    const qtdPessoa = totalConta / numPessoa
-    let checkPerson = qtdPessoa * (comissaoCustom / 100)
-    let checkTotal = (qtdPessoa * (comissaoCustom / 100)) + qtdPessoa
-
-    sumPerson = document.querySelector('#tip-person').innerHTML = checkPerson.toFixed(2)
-    totalPerson = document.querySelector('#total').innerHTML = checkTotal.toFixed(2)
+comissaoCustom.addEventListener('focusout', () => {
+    const comissao = parseFloat(comissaoCustom.value) / 100;
+    calcularValores(comissao);
 })
